4c72823c7a7bf1ac7aeadbddc204d0564443f2b3
Aditya Ghosh <adityaghosh5025@gmail.com>
2023-12-01 14:10:06 +0530

    going all the way

--- day_1/src/bin/part1.rs
+++ day_1/src/bin/part1.rs
@@ -10,14 +10,13 @@ fn main() {
 }
 
 fn part1(data: &str) -> i32 {
-    let mut result = 0;
-    for line in data.lines() {
-        let first_digit = line.chars().find(|c| c.is_numeric()).unwrap();
-        let last_digit = line.chars().rev().find(|c| c.is_numeric()).unwrap();
-        let num = format!("{first_digit}{last_digit}").parse::<i32>().unwrap();
-        result += num;
-    }
-    result
+    data.lines()
+        .map(|line| {
+            let first_digit = line.chars().find(|c| c.is_numeric()).unwrap();
+            let last_digit = line.chars().rev().find(|c| c.is_numeric()).unwrap();
+            format!("{first_digit}{last_digit}").parse::<i32>().unwrap()
+        })
+        .sum()
 }
 
 #[cfg(test)]
--- day_1/src/bin/part2.rs
+++ day_1/src/bin/part2.rs
@@ -9,8 +9,19 @@ fn main() {
     println!("Part 2: {output}");
 }
 
-fn part2(data: &str) -> String {
-    String::new()
+fn part1(data: &str) -> i32 {
+    let mut result = 0;
+    for line in data.lines() {
+        let first_digit = line.chars().find(|c| c.is_numeric()).unwrap();
+        let last_digit = line.chars().rev().find(|c| c.is_numeric()).unwrap();
+        let num = format!("{first_digit}{last_digit}").parse::<i32>().unwrap();
+        result += num;
+    }
+    result
+}
+
+fn part2(data: &str) -> i32 {
+    todo!()
 }
 
 #[cfg(test)]
@@ -25,8 +36,8 @@ mod tests {
     #[test]
     fn part2_works() {
         let data = load_file();
-        let first_half = first_half(&data);
+        let first_half = part2(&data);
 
-        assert_eq!(first_half, "");
+        assert_eq!(first_half, 281);
     }
 }
