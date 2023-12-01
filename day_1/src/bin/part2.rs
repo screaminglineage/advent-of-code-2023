83d66183426105fbbf322317726f862b1d68083b
Aditya Ghosh <adityaghosh5025@gmail.com>
2023-12-01 14:45:43 +0530

    part2 done

--- day_1/src/bin/part2.rs
+++ day_1/src/bin/part2.rs
@@ -9,19 +9,32 @@ fn main() {
     println!("Part 2: {output}");
 }
 
-fn part1(data: &str) -> i32 {
+const DIGITS: [&str; 9] = [
+    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+];
+
+fn part2(data: &str) -> u32 {
     let mut result = 0;
     for line in data.lines() {
-        let first_digit = line.chars().find(|c| c.is_numeric()).unwrap();
-        let last_digit = line.chars().rev().find(|c| c.is_numeric()).unwrap();
-        let num = format!("{first_digit}{last_digit}").parse::<i32>().unwrap();
-        result += num;
+        let mut digits_in_line: Vec<u32> = Vec::new();
+        let mut buf = String::new();
+        for ch in line.chars() {
+            if ch.is_numeric() {
+                digits_in_line.push(ch.to_digit(10).unwrap());
+                continue;
+            }
+
+            buf.push(ch);
+            for (i, digit) in DIGITS.iter().enumerate() {
+                if buf.contains(digit) {
+                    digits_in_line.push((i + 1) as u32);
+                    buf.drain(..buf.len() - 1);
+                }
+            }
+        }
+        result += digits_in_line[0] * 10 + digits_in_line.last().unwrap();
     }
     result
-}
-
-fn part2(data: &str) -> i32 {
-    todo!()
 }
 
 #[cfg(test)]
@@ -29,7 +42,6 @@ mod tests {
     use super::*;
 
     fn load_file() -> String {
-        use std::fs;
         fs::read_to_string(TEST_DATA_FILE).unwrap()
     }
 
