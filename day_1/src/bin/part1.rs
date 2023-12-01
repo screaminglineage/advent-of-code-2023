0afc625449165077875612c6aa581ffe037b8c64
Aditya Ghosh <adityaghosh5025@gmail.com>
2023-12-01 14:03:26 +0530

    part1 done

--- day_1/src/bin/part1.rs
+++ day_1/src/bin/part1.rs
@@ -9,8 +9,15 @@ fn main() {
     println!("Part 1: {output}");
 }
 
-fn part1(data: &str) -> String {
-    String::new()
+fn part1(data: &str) -> i32 {
+    let mut result = 0;
+    for line in data.lines() {
+        let first_digit = line.chars().find(|c| c.is_numeric()).unwrap();
+        let last_digit = line.chars().rev().find(|c| c.is_numeric()).unwrap();
+        let num = format!("{first_digit}{last_digit}").parse::<i32>().unwrap();
+        result += num;
+    }
+    result
 }
 
 #[cfg(test)]
@@ -18,15 +25,14 @@ mod tests {
     use super::*;
 
     fn load_file() -> String {
-        use std::fs;
         fs::read_to_string(TEST_DATA_FILE).unwrap()
     }
 
     #[test]
     fn part1_works() {
         let data = load_file();
-        let first_half = first_half(&data);
+        let first_half = part1(&data);
 
-        assert_eq!(first_half, "");
+        assert_eq!(first_half, 142);
     }
 }
