d6dccbc7e9933e07cd6ab91ce0d97ba9ae4ec3e5
Aditya Ghosh <adityaghosh5025@gmail.com>
2023-12-04 14:09:22 +0530

    part2 done

--- day_4/src/bin/part2.rs
+++ day_4/src/bin/part2.rs
@@ -1,6 +1,8 @@
+use std::collections::{HashMap, VecDeque};
 use std::fs;
 
 const DATA_FILE: &str = "data.txt";
+const MAX_CARD: u32 = 219;
 
 fn main() {
     let input = fs::read_to_string(DATA_FILE).unwrap();
@@ -17,27 +19,72 @@ fn nums_from_line<'a, I: Iterator<Item = &'a str>>(nums_it: &mut I) -> Vec<u32> 
         .collect::<Vec<u32>>()
 }
 
+fn get_card_num(card_line: &str) -> u32 {
+    card_line
+        .split(": ")
+        .next()
+        .unwrap()
+        .split_whitespace()
+        .last()
+        .unwrap()
+        .parse::<u32>()
+        .unwrap()
+}
+
+#[derive(Debug, Clone)]
+struct Card {
+    card_id: u32,
+    card_nums: Vec<u32>,
+    winnning_nums: Vec<u32>,
+    count: u32,
+}
+
+fn parse_card(card_line: &str) -> Card {
+    let card_id = get_card_num(card_line);
+    let nums = card_line.split(": ").last().unwrap();
+    let mut nums_it = nums.split("| ").into_iter();
+    let card_nums = nums_from_line(&mut nums_it);
+    let winnning_nums = nums_from_line(&mut nums_it);
+    Card {
+        card_id,
+        card_nums,
+        winnning_nums,
+        count: 1,
+    }
+}
+
+fn get_win_count(card: &Card) -> u32 {
+    card.card_nums
+        .iter()
+        .filter(|num| card.winnning_nums.contains(num))
+        .count() as u32
+}
+
+// damn this is so slow
 fn part2(data: &str) -> u32 {
-    data.lines()
-        .map(|card| {
-            let nums = card.split(": ").last().unwrap();
-            let mut nums_it = nums.split("| ").into_iter();
-            let card_nums = nums_from_line(&mut nums_it);
-            let winnning_nums = nums_from_line(&mut nums_it);
-            println!("{card_nums:?} {winnning_nums:?}\n");
+    let mut cards: HashMap<u32, Card> = HashMap::new();
+    let mut cards_to_process: VecDeque<u32> = VecDeque::new();
+    for card in data.lines() {
+        let card = parse_card(card);
+        let win_count = get_win_count(&card);
+        cards.insert(card.card_id, card.clone());
 
-            let win_count = card_nums
-                .iter()
-                .filter(|num| winnning_nums.contains(num))
-                .count() as u32;
+        let cards_won = card.card_id + 1..card.card_id + win_count + 1;
+        cards_to_process.extend(cards_won);
+    }
 
-            if win_count == 0 {
-                0
-            } else {
-                2u32.pow(win_count - 1)
-            }
-        })
-        .sum()
+    while !cards_to_process.is_empty() {
+        let card_id = cards_to_process.pop_front().unwrap();
+        let card = cards.get(&card_id).unwrap();
+
+        let win_count = get_win_count(&card);
+        cards.entry(card_id).and_modify(|card| card.count += 1);
+
+        let cards_won = card_id + 1..card_id + win_count + 1;
+        cards_to_process.extend(cards_won);
+    }
+
+    cards.values().map(|card| card.count).sum::<u32>()
 }
 
 #[cfg(test)]
@@ -50,7 +97,7 @@ mod tests {
     }
 
     #[test]
-    fn part1_works() {
+    fn part2_works() {
         let data = load_file();
         let output = part2(&data);
 
