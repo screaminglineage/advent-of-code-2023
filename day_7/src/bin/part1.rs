92ee79ac5a69889385d52000c69a8b79fd587a73
Aditya Ghosh <adityaghosh5025@gmail.com>
2023-12-07 14:56:49 +0530

    some parts done

--- day_7/src/bin/part1.rs
+++ day_7/src/bin/part1.rs
@@ -1,4 +1,6 @@
-use std::fs;
+use std::{collections::HashMap, fs};
+
+use itertools::Itertools;
 
 const DATA_FILE: &str = "data.txt";
 
@@ -8,8 +10,100 @@ fn main() {
     println!("Part 1: {output}");
 }
 
-fn part1(data: &str) -> String {
-    String::new()
+#[derive(Debug, Copy, Clone)]
+struct Hand<'a> {
+    cards: &'a str,
+    bid: u32,
+}
+
+fn count_cards(hand: &Hand) -> HashMap<char, u8> {
+    let mut card_count: HashMap<char, u8> = HashMap::new();
+    for card in hand.cards.chars() {
+        card_count.entry(card).and_modify(|c| *c += 1).or_insert(1);
+    }
+    card_count
+}
+
+fn part1(data: &str) -> u32 {
+    let hands: Vec<Hand> = data
+        .lines()
+        .map(|line| {
+            let line: Vec<&str> = line.split_whitespace().collect();
+            Hand {
+                cards: line[0],
+                bid: line[1].parse::<u32>().unwrap(),
+            }
+        })
+        .collect();
+
+    let five_of_kinds: Vec<&Hand> = hands
+        .iter()
+        .filter(|hand| {
+            let card_count = count_cards(hand);
+            card_count.values().any(|c| *c == 5)
+        })
+        .collect();
+
+    let four_of_kinds: Vec<&Hand> = hands
+        .iter()
+        .filter(|hand| {
+            let card_count = count_cards(hand);
+            card_count.values().any(|c| *c == 4)
+        })
+        .collect();
+
+    let full_houses: Vec<&Hand> = hands
+        .iter()
+        .filter(|hand| {
+            let card_count = count_cards(hand);
+            card_count.values().any(|c| *c == 3)
+                && card_count.values().filter(|c| **c != 3).all(|c| *c == 2)
+        })
+        .collect();
+
+    let three_of_kinds: Vec<&Hand> = hands
+        .iter()
+        .filter(|hand| {
+            let card_count = count_cards(hand);
+            card_count.values().any(|c| *c == 3)
+                && card_count.values().filter(|c| **c != 3).all(|c| *c == 1)
+        })
+        .collect();
+
+    let two_pairs: Vec<&Hand> = hands
+        .iter()
+        .filter(|hand| {
+            let card_count = count_cards(hand);
+            card_count.values().filter(|c| **c == 2).count() == 2
+        })
+        .collect();
+
+    let one_pairs: Vec<&Hand> = hands
+        .iter()
+        .filter(|hand| {
+            let card_count = count_cards(hand);
+            card_count.values().filter(|c| **c == 2).count() == 1
+                && card_count.values().filter(|c| **c == 1).count() == 3
+        })
+        .collect();
+
+    let high_cards: Vec<&Hand> = hands
+        .iter()
+        .filter(|hand| {
+            let card_count = count_cards(hand);
+            card_count.values().all(|c| *c == 1)
+        })
+        .collect();
+
+    dbg!(five_of_kinds);
+    dbg!(four_of_kinds);
+    dbg!(full_houses);
+    dbg!(three_of_kinds);
+    dbg!(two_pairs);
+    dbg!(one_pairs);
+    dbg!(high_cards);
+
+    0
 }
 
 #[cfg(test)]
@@ -26,6 +120,6 @@ mod tests {
         let data = load_file();
         let output = part1(&data);
 
-        assert_eq!(output, "");
+        assert_eq!(output, 6440);
     }
 }
