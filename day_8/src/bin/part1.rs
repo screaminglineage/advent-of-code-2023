97bdd3d71ac6d0e9cd7e20fb57ca939d4a4db04c
Aditya Ghosh <adityaghosh5025@gmail.com>
2023-12-08 12:17:53 +0530

    part1 done

--- day_8/src/bin/part1.rs
+++ day_8/src/bin/part1.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{collections::HashMap, fs, panic::Location};
 
 const DATA_FILE: &str = "data.txt";
 
@@ -8,8 +8,43 @@ fn main() {
     println!("Part 1: {output}");
 }
 
-fn part1(data: &str) -> String {
-    String::new()
+fn part1(data: &str) -> u32 {
+    let instructions = data.lines().next().unwrap().chars();
+    let mut elements: HashMap<&str, (&str, &str)> = HashMap::new();
+
+    for line in data.lines().skip(2) {
+        let location = line.split(" = (").next().unwrap();
+        let directions: (&str, &str) = line
+            .split(" = (")
+            .last()
+            .unwrap()
+            .split(")")
+            .next()
+            .unwrap()
+            .split_once(", ")
+            .unwrap();
+
+        elements.insert(location, directions);
+    }
+
+    let mut curr = "AAA";
+    let end = "ZZZ";
+    let mut count = 0;
+    let mut instruct_it = instructions.cycle();
+
+    loop {
+        if curr == end {
+            return count;
+        }
+
+        let directions = elements.get(&curr).unwrap();
+        curr = match instruct_it.next() {
+            Some('L') => directions.0,
+            Some('R') => directions.1,
+            _ => unimplemented!(),
+        };
+        count += 1;
+    }
 }
 
 #[cfg(test)]
@@ -26,6 +61,6 @@ mod tests {
         let data = load_file();
         let output = part1(&data);
 
-        assert_eq!(output, "");
+        assert_eq!(output, 2);
     }
 }
