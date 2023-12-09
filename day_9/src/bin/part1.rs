f85246488e9f3d86f17d0e663a90becbd572365d
Aditya Ghosh <adityaghosh5025@gmail.com>
2023-12-09 17:23:31 +0530

    part1 done

--- day_9/src/bin/part1.rs
+++ day_9/src/bin/part1.rs
@@ -8,8 +8,30 @@ fn main() {
     println!("Part 1: {output}");
 }
 
-fn part1(data: &str) -> String {
-    String::new()
+fn part1(data: &str) -> i32 {
+    data.lines()
+        .map(|line| {
+            let mut nums: Vec<i32> = line.split(" ").map(|n| n.parse().unwrap()).collect();
+
+            let mut prev_nums: Vec<Vec<i32>> = vec![nums.clone()];
+            while !nums.iter().all(|&n| n == 0) {
+                nums = nums
+                    .iter()
+                    .zip(nums.iter().skip(1))
+                    .map(|(x, y)| y - x)
+                    .collect();
+                prev_nums.push(nums.clone());
+            }
+
+            for i in (1..prev_nums.len()).rev() {
+                let curr = *prev_nums[i].last().unwrap();
+                let prev = *prev_nums[i - 1].last().unwrap();
+                prev_nums[i - 1].push(curr + prev);
+            }
+
+            *prev_nums.iter().next().unwrap().last().unwrap()
+        })
+        .sum()
 }
 
 #[cfg(test)]
@@ -26,6 +48,6 @@ mod tests {
         let data = load_file();
         let output = part1(&data);
 
-        assert_eq!(output, "");
+        assert_eq!(output, 114);
     }
 }
