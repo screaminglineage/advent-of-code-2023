95e16add18d8acc49d0f1d8f76fede3bcdeda443
Aditya Ghosh <adityaghosh5025@gmail.com>
2023-12-13 15:33:19 +0530

    still not over

--- day_13/src/bin/part1.rs
+++ day_13/src/bin/part1.rs
@@ -8,36 +8,45 @@ fn main() {
     println!("Part 1: {output}");
 }
 
-fn detect_mirror(pattern: &str) -> u32 {
-    let pattern: Vec<&str> = pattern.lines().collect();
+fn detect_mirror(pattern: &[Vec<char>]) -> Option<u32> {
     let indices: Vec<_> = pattern
         .windows(2)
         .enumerate()
-        .filter(|(_, pair)| pair[0].chars().eq(pair[1].chars()))
+        .filter(|(_, pair)| pair[0].iter().eq(pair[1].iter()))
         .map(|(i, _)| i)
         .collect();
-    dbg!(pattern.len());
-    0
+
+    for index in indices {
+        let (p1, p2) = pattern.split_at(index);
+        if p1.iter().zip(p2.iter()).all(|(x, y)| x == y) {
+            return Some(index as u32);
+        }
+    }
+    None
 }
 
 fn count_rows_above(pattern: &str) -> u32 {
     let pattern: Vec<&str> = pattern.lines().collect();
     let mut front = 0;
     let mut back = pattern.len() - 1;
-    let median = dbg!(back / 2);
+    let median = back / 2;
     let mut count = 0;
+
+    let mut i = 0;
     while front <= median && back > median {
         if pattern[front] == pattern[back] {
             count += 1;
             front += 1;
             back -= 1;
-        } else if pattern[front] == pattern[back - 1] {
+        } else if i == 0 && pattern[front] == pattern[back - 1] {
             back -= 1;
-        } else if pattern[front + 1] == pattern[back] {
+        } else if i == 0 && pattern[front + 1] == pattern[back] {
             front += 1;
         } else {
+            count = 0;
             break;
         }
+        i += 1;
     }
 
     if count == 0 {
@@ -47,10 +56,67 @@ fn count_rows_above(pattern: &str) -> u32 {
     }
 }
 
+fn count_rows_above_2(pattern: &[Vec<char>]) -> u32 {
+    let mut front = 0;
+    let mut back = pattern.len() - 1;
+    let median = back / 2;
+    let mut count = 0;
+
+    let mut i = 0;
+    while front <= median && back > median {
+        if pattern[front] == pattern[back] {
+            count += 1;
+            front += 1;
+            back -= 1;
+        } else if i == 0 && pattern[front] == pattern[back - 1] {
+            back -= 1;
+        } else if i == 0 && pattern[front + 1] == pattern[back] {
+            front += 1;
+        } else {
+            count = 0;
+            break;
+        }
+        i += 1;
+    }
+
+    if count == 0 {
+        count
+    } else {
+        count + 1
+    }
+}
+
+fn rotate_90<T>(v: Vec<Vec<T>>) -> Vec<Vec<T>> {
+    assert!(!v.is_empty());
+    let len = v[0].len();
+    let mut iters: Vec<_> = v.into_iter().map(|n| n.into_iter()).collect();
+    (0..len)
+        .map(|_| {
+            iters
+                .iter_mut()
+                .map(|n| n.next().unwrap())
+                .rev()
+                .collect::<Vec<T>>()
+        })
+        .rev()
+        .collect()
+}
+
 fn part1(data: &str) -> u32 {
     let patterns: Vec<&str> = data.split("\n\n").collect();
-    let a = count_rows_above(patterns[0]);
-    dbg!(a);
+    for p in &patterns {
+        let a: Vec<Vec<char>> = p.lines().map(|line| line.chars().collect()).collect();
+
+        let n = detect_mirror(&a);
+        let a = rotate_90(a);
+        let r = detect_mirror(&a);
+        // for i in &a {
+        //     println!("{i:?}")
+        // }
+
+        println!("not rotated: {:?}, rotated: {:?}", n, r);
+    }
+
     0
 }
 
