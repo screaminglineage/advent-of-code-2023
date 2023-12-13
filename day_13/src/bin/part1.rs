5f15789d980785544d36ef6b19d87f2dbd2ba3f0
Aditya Ghosh <adityaghosh5025@gmail.com>
2023-12-13 12:14:48 +0530

    added files

---
+++ day_13/src/bin/part1.rs
@@ -0,0 +1,31 @@
+use std::fs;
+
+const DATA_FILE: &str = "data.txt";
+
+fn main() {
+    let input = fs::read_to_string(DATA_FILE).unwrap();
+    let output = part1(&input);
+    println!("Part 1: {output}");
+}
+
+fn part1(data: &str) -> u32 {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    const TEST_DATA_FILE: &str = "test_data.txt";
+
+    fn load_file() -> String {
+        fs::read_to_string(TEST_DATA_FILE).unwrap()
+    }
+
+    #[test]
+    fn part1_works() {
+        let data = load_file();
+        let output = part1(&data);
+
+        assert_eq!(output, 405);
+    }
+}
