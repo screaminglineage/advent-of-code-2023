52053e831c739bea4cbb11b3aaee13df453257a0
Aditya Ghosh <adityaghosh5025@gmail.com>
2023-12-13 14:15:29 +0530

    added function to check mirror

--- day_13/src/bin/part1.rs
+++ day_13/src/bin/part1.rs
@@ -8,7 +8,49 @@ fn main() {
     println!("Part 1: {output}");
 }
 
+fn detect_mirror(pattern: &str) -> u32 {
+    let pattern: Vec<&str> = pattern.lines().collect();
+    let indices: Vec<_> = pattern
+        .windows(2)
+        .enumerate()
+        .filter(|(_, pair)| pair[0].chars().eq(pair[1].chars()))
+        .map(|(i, _)| i)
+        .collect();
+    dbg!(pattern.len());
+    0
+}
+
+fn count_rows_above(pattern: &str) -> u32 {
+    let pattern: Vec<&str> = pattern.lines().collect();
+    let mut front = 0;
+    let mut back = pattern.len() - 1;
+    let median = dbg!(back / 2);
+    let mut count = 0;
+    while front <= median && back > median {
+        if pattern[front] == pattern[back] {
+            count += 1;
+            front += 1;
+            back -= 1;
+        } else if pattern[front] == pattern[back - 1] {
+            back -= 1;
+        } else if pattern[front + 1] == pattern[back] {
+            front += 1;
+        } else {
+            break;
+        }
+    }
+
+    if count == 0 {
+        count
+    } else {
+        count + 1
+    }
+}
+
 fn part1(data: &str) -> u32 {
+    let patterns: Vec<&str> = data.split("\n\n").collect();
+    let a = count_rows_above(patterns[0]);
+    dbg!(a);
     0
 }
 
