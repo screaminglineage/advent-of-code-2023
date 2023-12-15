5f971ed3088cc10f415189da3dea31601fb10256
Aditya Ghosh <adityaghosh5025@gmail.com>
2023-12-15 13:51:09 +0530

    part2 partially complete

--- day_15/src/bin/part2.rs
+++ day_15/src/bin/part2.rs
@@ -8,6 +8,12 @@ fn main() {
     println!("part 2: {output}");
 }
 
+#[derive(Copy, Clone, Debug)]
+struct Lens<'a> {
+    label: &'a str,
+    focal_len: u32,
+}
+
 fn calculate_hash(seq: &str) -> u32 {
     let mut curr = 0;
     for ch in seq.chars() {
@@ -18,9 +24,46 @@ fn calculate_hash(seq: &str) -> u32 {
     curr
 }
 
+const VAL: Vec<Lens> = vec![];
+
 fn part2(data: &str) -> u32 {
     let data: Vec<&str> = data.trim().split(',').collect();
-    data.into_iter().map(calculate_hash).sum()
+    let mut hashmap: [Vec<Lens>; 256] = [VAL; 256];
+
+    for lens_data in data {
+        let lens_data_parsed = lens_data.split(['-', '=']).collect::<Vec<&str>>();
+        dbg!(&lens_data_parsed);
+        let lens = Lens {
+            label: lens_data_parsed[0],
+            focal_len: lens_data_parsed[1].parse().unwrap_or_default(),
+        };
+
+        let hash = calculate_hash(lens.label) as usize;
+
+        if lens_data.contains(&"-") {
+            if let Some(i) = hashmap[hash]
+                .iter()
+                .position(|inner_lens| inner_lens.label == lens.label)
+            {
+                hashmap[hash].remove(i);
+            }
+        } else if lens_data.contains(&"=") {
+            match hashmap[hash]
+                .iter()
+                .position(|inner_lens| inner_lens.label == lens.label)
+            {
+                // unwrapping is safe as we know i already exists
+                Some(i) => *hashmap[hash].get_mut(i).unwrap() = lens,
+                None => hashmap[hash].push(lens),
+            }
+        } else {
+            unreachable!("only options are - and =");
+        }
+    }
+
+    dbg!(&hashmap[0]);
+
+    0
 }
 
 #[cfg(test)]
