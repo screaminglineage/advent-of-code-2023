718c20bd87311fe4580fecf89ea28c89884df3fb
Aditya Ghosh <adityaghosh5025@gmail.com>
2023-12-16 13:24:21 +0530

    added basic structure

--- day_16/src/bin/part1.rs
+++ day_16/src/bin/part1.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{collections::HashSet, fs};
 
 const DATA_FILE: &str = "data.txt";
 
@@ -8,7 +8,62 @@ fn main() {
     println!("Part 1: {output}");
 }
 
+enum Directions {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+struct Point {
+    y: i32,
+    x: i32,
+}
+
+struct Ray {
+    point: Point,
+    direction: Directions,
+}
+
+use Directions::*;
+
+fn move_rays(grid: &[Vec<char>], energized: &mut HashSet<Point>, rays: Vec<Ray>) -> Vec<Ray> {
+    let max_y = grid.len() as i32;
+    let max_x = grid[0].len() as i32;
+
+    let new_rays = Vec::new();
+
+    for mut ray in rays {
+        match ray.direction {
+            Up => ray.point.y -= 1,
+            Down => ray.point.y += 1,
+            Left => ray.point.x -= 1,
+            Right => ray.point.x += 1,
+        }
+
+        // out of bounds
+        if ray.point.y >= max_y || ray.point.x >= max_x || ray.point.y < 0 || ray.point.x < 0 {
+            continue;
+        }
+
+        match ray.direction {
+            _ => (),
+        }
+
+        new_rays.push(ray);
+    }
+    new_rays
+}
+
 fn part1(data: &str) -> u32 {
+    let grid: Vec<Vec<char>> = data.lines().map(|line| line.chars().collect()).collect();
+    let mut energized: HashSet<Point> = HashSet::new();
+    let mut rays: Vec<Ray> = Vec::new();
+    rays.push(Ray {
+        point: Point { y: 0, x: 0 },
+        direction: Right,
+    });
+    let rays = move_rays(&grid, &mut energized, rays);
     0
 }
 
