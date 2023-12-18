dd5213ecda3360ee7daf3badcca1f07208e564b5
Aditya Ghosh <adityaghosh5025@gmail.com>
2023-12-18 13:43:50 +0530

    parsed input data

--- day_18/src/bin/part1.rs
+++ day_18/src/bin/part1.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{collections::HashSet, fs};
 
 const DATA_FILE: &str = "data.txt";
 
@@ -8,7 +8,42 @@ fn main() {
     println!("Part 1: {output}");
 }
 
+fn calc_edges(directions: &[(char, u32)]) -> u32 {
+    let mut total = 0;
+    let visited: HashSet<(i32, i32)> = HashSet::new();
+    let mut current = (0, 0);
+    for (dir, count) in directions {
+        match dir {
+            'U' => (),
+            'D' => (),
+            'L' => (),
+            'R' => (),
+            _ => unreachable!(),
+        }
+    }
+    total
+}
+
 fn part1(data: &str) -> u32 {
+    let directions: Vec<(char, u32)> = data
+        .lines()
+        .map(|line| {
+            let mut l = line.split_whitespace();
+            (
+                l.next().unwrap().chars().next().unwrap(),
+                l.next()
+                    .unwrap()
+                    .chars()
+                    .next()
+                    .unwrap()
+                    .to_digit(10)
+                    .unwrap(),
+            )
+        })
+        .collect();
+
+    dbg!(calc_edges(&directions));
+
     0
 }
 
@@ -24,7 +59,7 @@ mod tests {
     #[test]
     fn outer_edge() {
         let data = load_file();
-        let output = todo!();
+        let output = calc_edges(&[('A', 32)]);
 
         assert_eq!(output, 38);
     }
